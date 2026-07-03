97d651e82f5b81135ee77b3621db004f8f35f991
RobinWieja <r.wieja@ille.de>
2026-07-03 15:23:59 +0200

    a e a aa axxa iie oeoeo  aa aieieuu oo ja statischer pfad ersetzt mit dynamischen selection auf runtimeee instance ssccshcchhould work hshshashahhhahahahaheeeheee  nnnnnn ^^^^^^ xdxdxoxox67 commit push

--- Clicker_Generator.editor/src-gen/clicker_Generator/presentation/GeneratorTest.java
+++ Clicker_Generator.editor/src-gen/clicker_Generator/presentation/GeneratorTest.java
@@ -7,10 +7,16 @@ import java.nio.file.Path;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import clicker_Generator.game;
 
@@ -29,19 +35,16 @@ public class GeneratorTest extends AbstractHandler{
 	        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
 	            .put("clicker_generator", new XMIResourceFactoryImpl());
 
-	        Resource resource = resourceSet.getResource(
-	            org.eclipse.emf.common.util.URI.createFileURI(
-	                "My.clicker_generator"), true);
+	        IFile file = (IFile) ((IStructuredSelection) HandlerUtil.getCurrentSelection(event)).getFirstElement();
+	        URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+
+	        Resource resource = resourceSet.getResource(uri, true);
 
 	        game game = (game) resource.getContents().get(0);
 
 	        String html = new ClickerHtmlGenerator().generate(game);
-	        try {
-				Files.writeString(Path.of("output/cookieClicker.html"), html);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+	        System.out.println(html);
 
 	        System.out.println("Fertig! Öffne output/cookieClicker.html im Browser.");
 		return null;
