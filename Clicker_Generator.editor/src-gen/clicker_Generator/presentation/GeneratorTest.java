0c487039d2ed30aaa6c0f01bf1a4048f8df8e306
Pinkou <leila.chantal.schuhmacher@gmail.com>
2026-07-08 14:51:50 +0200

    ja funktioniert doch..
      ich befinde mich in einem zeitalter der unsicherheit.... in einem zeit
    der zeit hab ich ein midllife cwisis seit beginn meines lebens. manchmal
    frage ich mich wofür? wieso? weshalb? warum? danke iblali

--- Clicker_Generator.editor/src-gen/clicker_Generator/presentation/GeneratorTest.java
+++ Clicker_Generator.editor/src-gen/clicker_Generator/presentation/GeneratorTest.java
@@ -12,41 +12,66 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 
 import clicker_Generator.game;
 
-public class GeneratorTest extends AbstractHandler{
-    public static void main(String[] args) throws Exception {
-        // Wichtig bei Standalone-Ausführung: Package + Resource-Factory registrieren
- 
-    }
+public class GeneratorTest extends AbstractHandler {
+	public static void main(String[] args) throws Exception {
+		// Wichtig bei Standalone-Ausführung: Package + Resource-Factory registrieren
+
+	}
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
-	       clicker_Generator.Clicker_GeneratorPackage.eINSTANCE.eClass(); // lädt das Metamodell
-	        
-	        ResourceSet resourceSet = new ResourceSetImpl();
-	        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
-	            .put("clicker_generator", new XMIResourceFactoryImpl());
+        public Object execute(ExecutionEvent event) throws ExecutionException {
+                // TODO Auto-generated method stub
+               clicker_Generator.Clicker_GeneratorPackage.eINSTANCE.eClass();
+// lädt das Metamodell
 
-	        IFile file = (IFile) ((IStructuredSelection) HandlerUtil.getCurrentSelection(event)).getFirstElement();
-	        URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+                ResourceSet resourceSet = new ResourceSetImpl();
 
-	        Resource resource = resourceSet.getResource(uri, true);
+resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+                    .put("clicker_generator", new XMIResourceFactoryImpl());
 
-	        game game = (game) resource.getContents().get(0);
+                IFile file = (IFile) ((IStructuredSelection)
+HandlerUtil.getCurrentSelection(event)).getFirstElement();
+                URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
 
-	        String html = new ClickerHtmlGenerator().generate(game);
+                Resource resource = resourceSet.getResource(uri, true);
 
-	        System.out.println(html);
+                game game = (game) resource.getContents().get(0);
 
-	        System.out.println("Fertig! Öffne output/cookieClicker.html im Browser.");
-		return null;
-	}
+                String html = new ClickerHtmlGenerator().generate(game);
+
+             // Build path: <project>/output/cookieClicker.html
+             IProject project = file.getProject();
+             IFolder outputFolder = project.getFolder("output");
+             try {
+                    if (!outputFolder.exists()) {
+                        outputFolder.create(true, true, null);
+                    }
+
+                    Path outputPath =
+Paths.get(outputFolder.getLocation().toOSString(), "clickerGame.html");
+                    Files.write(outputPath,
+html.getBytes(StandardCharsets.UTF_8));
+
+                    outputFolder.refreshLocal(IResource.DEPTH_INFINITE, null);
+                } catch (CoreException e) {
+                    throw new ExecutionException("Failed to write generated HTML output", e);
+                } catch (IOException e) {
+                    throw new ExecutionException("Failed to write generated HTML file", e);
+                }
+
+             System.out.println("Fertig! Öffne output/clickerGame.html im Browser.");
+                return null;
+        }
 }
